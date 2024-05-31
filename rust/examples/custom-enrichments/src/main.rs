57e23a38e3889a20b235cab55fed519ee212fa64
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-05-31 16:09:48 +0200

    added more functions

--- rust/examples/custom-enrichments/src/main.rs
+++ rust/examples/custom-enrichments/src/main.rs
@@ -1,13 +1,15 @@
 use anyhow::Result;
-use cx_sdk::com::coralogix::enrichment::v1::CreateCustomEnrichmentResponse;
-use cx_sdk::com::coralogix::enrichment::v1::{File, file::Content as FileContent, Enrichment};
-use cx_sdk::com::coralogix::enrichment::v1::enrichment_service_client::EnrichmentServiceClient;
 use cx_sdk::com::coralogix::enrichment::v1::custom_enrichment_service_client::CustomEnrichmentServiceClient;
-use cx_sdk::com::coralogix::enrichment::v1::CreateCustomEnrichmentRequest;
+use cx_sdk::com::coralogix::enrichment::v1::enrichment_service_client::EnrichmentServiceClient;
 use cx_sdk::com::coralogix::enrichment::v1::enrichment_type::Type;
+use cx_sdk::com::coralogix::enrichment::v1::{CreateCustomEnrichmentRequest, CustomEnrichment, DeleteCustomEnrichmentResponse, Enrichment, GetCustomEnrichmentRequest, GetCustomEnrichmentsRequest};
+use cx_sdk::com::coralogix::enrichment::v1::{file::Content as FileContent, File};
 use cx_sdk::com::coralogix::enrichment::v1::{
     AddEnrichmentsRequest, CustomEnrichmentType, EnrichmentRequestModel, EnrichmentType,
     GetEnrichmentsRequest, RemoveEnrichmentsRequest,
+};
+use cx_sdk::com::coralogix::enrichment::v1::{
+    CreateCustomEnrichmentResponse, DeleteCustomEnrichmentRequest,
 };
 use cx_sdk::{ApiKey, AuthData};
 use std::str::FromStr;
@@ -15,7 +17,7 @@ use tokio::sync::Mutex;
 use tonic::{
     metadata::MetadataMap,
     transport::{Channel, Endpoint},
-    Code, Request, Status,
+    Request,
 };
 
 pub fn make_request_with_metadata<T>(request: T, new_metadata: &MetadataMap) -> Request<T> {
@@ -25,13 +27,13 @@ pub fn make_request_with_metadata<T>(request: T, new_metadata: &MetadataMap) -> 
     req
 }
 
-pub struct ExternalEnrichmentService {
+pub struct EnrichmentService {
     metadata_map: MetadataMap,
     enrichment_key_client: Mutex<EnrichmentServiceClient<Channel>>,
     custom_enrichment_client: Mutex<CustomEnrichmentServiceClient<Channel>>,
 }
 
-impl ExternalEnrichmentService {
+impl EnrichmentService {
     pub fn new(endpoint: &str, api_key: String) -> Self {
         let enrichments_service_channel: Channel =
             Endpoint::from_str(endpoint).unwrap().connect_lazy();
@@ -48,7 +50,11 @@ impl ExternalEnrichmentService {
         }
     }
 
-    async fn create_enrichment(&self, name: String, data: String) -> Result<CreateCustomEnrichmentResponse> {
+    async fn create_enrichment(
+        &self,
+        name: String,
+        data: String,
+    ) -> Result<CreateCustomEnrichmentResponse> {
         let file = File {
             name: Some(name.clone()),
             content: Some(FileContent::Binary(data.into_bytes())),
@@ -72,17 +78,81 @@ impl ExternalEnrichmentService {
                 .map_err(From::from)
         }
     }
+    async fn delete_enrichment(
+        &self,
+        custom_enrichment_id: u32,
+    ) -> Result<DeleteCustomEnrichmentResponse> {
+        let request: Request<DeleteCustomEnrichmentRequest> = make_request_with_metadata(
+            DeleteCustomEnrichmentRequest {
+                custom_enrichment_id: Some(custom_enrichment_id),
+            },
+            &self.metadata_map,
+        );
+        {
+            let mut client = self.custom_enrichment_client.lock().await.clone();
 
-    async fn set_enrichment_key(&self, enrichment_id: u32) -> Result<()> {
+            client
+                .delete_custom_enrichment(request)
+                .await
+                .map(|r| r.into_inner())
+                .map_err(From::from)
+        }
+    }
+
+    pub async fn list_enrichment_keys(&self) -> Result<Vec<Enrichment>> {
+        let get_enrichments_request = GetEnrichmentsRequest {};
+        let request = make_request_with_metadata(get_enrichments_request, &self.metadata_map);
+        {
+            let mut client = self.enrichment_key_client.lock().await.clone();
+            let response = client.get_enrichments(request).await?;
+            Ok(response
+                .into_inner()
+                .enrichments
+                .into_iter()
+                .collect())
+        }
+    }
+
+    pub async fn get_enrichment(&self, id: u32) -> Result<Option<CustomEnrichment>> {
+        let get_enrichments_request = GetCustomEnrichmentRequest { id: Some(id)};
+        let request = make_request_with_metadata(get_enrichments_request, &self.metadata_map);
+        {
+            let mut client = self.custom_enrichment_client.lock().await.clone();
+            let response = client.get_custom_enrichment(request).await?;
+            Ok(response
+                .into_inner()
+                .custom_enrichment)
+        }
+    }
+
+    pub async fn get_enrichments(&self) -> Result<Vec<CustomEnrichment>> {
+        let get_enrichments_request = GetCustomEnrichmentsRequest { };
+        let request = make_request_with_metadata(get_enrichments_request, &self.metadata_map);
+        {
+            let mut client = self.custom_enrichment_client.lock().await.clone();
+            let response = client.get_custom_enrichments(request).await?;
+            Ok(response
+                .into_inner()
+                .custom_enrichments)
+        }
+    }
+
+
+    async fn add_enrichment_key_mapping(
+        &self,
+        enrichment_id: u32,
+        from: String,
+        to: String,
+    ) -> Result<()> {
         let add_enrichments_request = AddEnrichmentsRequest {
             request_enrichments: vec![EnrichmentRequestModel {
-                field_name: Some("input".to_string()),
+                field_name: Some(from),
                 enrichment_type: Some(EnrichmentType {
                     r#type: Some(Type::CustomEnrichment(CustomEnrichmentType {
                         id: Some(enrichment_id),
                     })),
                 }),
-                enriched_field_name: Some("enriched".to_string()),
+                enriched_field_name: Some(to),
             }],
         };
         let request: Request<AddEnrichmentsRequest> =
@@ -94,11 +164,7 @@ impl ExternalEnrichmentService {
         Ok(())
     }
 
-    async fn delete_enrichment_keys(
-        &self,
-        enrichment_ids: Vec<u32>,
-        _company_id: u32,
-    ) -> Result<()> {
+    async fn delete_enrichment_keys(&self, enrichment_ids: Vec<u32>) -> Result<()> {
         let remove_enrichments_request = RemoveEnrichmentsRequest { enrichment_ids };
         let request = make_request_with_metadata(remove_enrichments_request, &self.metadata_map);
         {
@@ -107,23 +173,22 @@ impl ExternalEnrichmentService {
         }
         Ok(())
     }
-
-    pub async fn get_ids(&self) -> Result<Vec<u32>> {
-        let get_enrichments_request = GetEnrichmentsRequest {};
-        let request = make_request_with_metadata(get_enrichments_request, &self.metadata_map);
-        {
-            let mut client = self.enrichment_key_client.lock().await.clone();
-            let response = client.get_enrichments(request).await?;
-            Ok(response
-                .into_inner()
-                .enrichments
-                .into_iter()
-                .map(|e| e.id)
-                .collect())
-        }
-    }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let svc = EnrichmentService::new(
+        "https://ng-api-grpc.eu2.coralogix.com".into(),
+        "my-api-key".into(),
+    );
+    let enrichment = svc.create_enrichment("my-enrichment".into(), "a,b,c\n1,2,3".into())
+        .await
+        .unwrap().custom_enrichment.unwrap();
+    let ids = svc.list_enrichment_keys().await.unwrap();
+    let enrichment_id = enrichment.id;
+    assert!(ids.iter().find(|e|e.id == enrichment.id).is_some());
+
+    svc.add_enrichment_key_mapping(enrichment_id, "from".into(), "to".into()).await.unwrap();
+    svc.delete_enrichment_keys(ids.iter().map(|e|e.id).collect()).await.unwrap();
+    let _ = svc.delete_enrichment(enrichment_id).await.unwrap();
 }
