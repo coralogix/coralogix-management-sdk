0dbac5622281d4b40f523bd28d4ecc4355ff7659
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-06-13 10:52:11 +0200

    more protos

--- go/cxsdk.go
+++ go/cxsdk.go
@@ -1,29 +1,25 @@
 package cxsdk
 
 type ClientSet struct {
-	ruleGroups          *RuleGroupsClient
-	alerts              *AlertsClient
-	enrichments         *EnrichmentsClient
-	dataSet             *DataSetClient
-	dashboards          *DashboardsClient
-	grafana             *GrafanaClient
-	actions             *ActionsClient
-	recordingRuleGroups *RecordingRulesGroupsSetsClient
-	tcoPolicies         *TCOPoliciesClient
-	webhooks            *WebhooksClient
-	events2Metrics      *Events2MetricsClient
-	slis                *SLIClient
-	archiveRetentions   *ArchiveRetentionsClient
-	archiveMetrics      *ArchiveMetricsClient
-	archiveLogs         *ArchiveLogsClient
-	alertsSchedulers    *AlertsSchedulersClient
-	teams               *TeamsClient
-	slos                *SLOsClient
-	dahboardsFolders    *DashboardsFoldersClient
-	apiKeys             *ApikeysClient
-	groups              *GroupsClient
-	users               *UsersClient
-	customRole          *RolesClient
+	ruleGroups        *RuleGroupsClient
+	alerts            *AlertsClient
+	enrichments       *EnrichmentsClient
+	dataSet           *DataSetClient
+	dashboards        *DashboardsClient
+	actions           *ActionsClient
+	tcoPolicies       *TCOPoliciesClient
+	webhooks          *WebhooksClient
+	events2Metrics    *Events2MetricsClient
+	slis              *SLIClient
+	archiveRetentions *ArchiveRetentionsClient
+	archiveMetrics    *ArchiveMetricsClient
+	archiveLogs       *ArchiveLogsClient
+	alertsSchedulers  *AlertsSchedulersClient
+	teams             *TeamsClient
+	slos              *SLOsClient
+	dahboardsFolders  *DashboardsFoldersClient
+	apiKeys           *ApikeysClient
+	users             *UsersClient
 }
 
 func (c *ClientSet) RuleGroups() *RuleGroupsClient {
@@ -46,16 +42,8 @@ func (c *ClientSet) Dashboards() *DashboardsClient {
 	return c.dashboards
 }
 
-func (c *ClientSet) Grafana() *GrafanaClient {
-	return c.grafana
-}
-
 func (c *ClientSet) Actions() *ActionsClient {
 	return c.actions
-}
-
-func (c *ClientSet) RecordingRuleGroupsSets() *RecordingRulesGroupsSetsClient {
-	return c.recordingRuleGroups
 }
 
 func (c *ClientSet) TCOPolicies() *TCOPoliciesClient {
@@ -98,20 +86,12 @@ func (c *ClientSet) ApiKeys() *ApikeysClient {
 	return c.apiKeys
 }
 
-func (c *ClientSet) CustomRoles() *RolesClient {
-	return c.customRole
-}
-
 func (c *ClientSet) SLOs() *SLOsClient {
 	return c.slos
 }
 
 func (c *ClientSet) DashboardsFolders() *DashboardsFoldersClient {
 	return c.dahboardsFolders
-}
-
-func (c *ClientSet) Groups() *GroupsClient {
-	return c.groups
 }
 
 func (c *ClientSet) Users() *UsersClient {
@@ -122,28 +102,24 @@ func NewClientSet(targetUrl, apiKey string) *ClientSet {
 	apikeyCPC := NewCallPropertiesCreator(targetUrl, apiKey)
 
 	return &ClientSet{
-		ruleGroups:          NewRuleGroupsClient(apikeyCPC),
-		alerts:              NewAlertsClient(apikeyCPC),
-		events2Metrics:      NewEvents2MetricsClient(apikeyCPC),
-		enrichments:         NewEnrichmentClient(apikeyCPC),
-		dataSet:             NewDataSetClient(apikeyCPC),
-		dashboards:          NewDashboardsClient(apikeyCPC),
-		grafana:             NewGrafanaClient(apikeyCPC),
-		actions:             NewActionsClient(apikeyCPC),
-		recordingRuleGroups: NewRecordingRuleGroupsClient(apikeyCPC),
-		tcoPolicies:         NewTCOPoliciesClient(apikeyCPC),
-		webhooks:            NewWebhooksClient(apikeyCPC),
-		slis:                NewSLIsClient(apikeyCPC),
-		archiveRetentions:   NewArchiveRetentionsClient(apikeyCPC),
-		archiveMetrics:      NewArchiveMetricsClient(apikeyCPC),
-		archiveLogs:         NewArchiveLogsClient(apikeyCPC),
-		alertsSchedulers:    NewAlertsSchedulersClient(apikeyCPC),
-		teams:               NewTeamsClient(apikeyCPC),
-		slos:                NewSLOsClient(apikeyCPC),
-		dahboardsFolders:    NewDashboardsFoldersClient(apikeyCPC),
-		apiKeys:             NewApiKeysClient(apikeyCPC),
-		groups:              NewGroupsClient(apikeyCPC),
-		users:               NewUsersClient(apikeyCPC),
-		customRole:          NewRolesClient(apikeyCPC),
+		ruleGroups:        NewRuleGroupsClient(apikeyCPC),
+		alerts:            NewAlertsClient(apikeyCPC),
+		events2Metrics:    NewEvents2MetricsClient(apikeyCPC),
+		enrichments:       NewEnrichmentClient(apikeyCPC),
+		dataSet:           NewDataSetClient(apikeyCPC),
+		dashboards:        NewDashboardsClient(apikeyCPC),
+		actions:           NewActionsClient(apikeyCPC),
+		tcoPolicies:       NewTCOPoliciesClient(apikeyCPC),
+		webhooks:          NewWebhooksClient(apikeyCPC),
+		slis:              NewSLIsClient(apikeyCPC),
+		archiveRetentions: NewArchiveRetentionsClient(apikeyCPC),
+		archiveMetrics:    NewArchiveMetricsClient(apikeyCPC),
+		archiveLogs:       NewArchiveLogsClient(apikeyCPC),
+		alertsSchedulers:  NewAlertsSchedulersClient(apikeyCPC),
+		teams:             NewTeamsClient(apikeyCPC),
+		slos:              NewSLOsClient(apikeyCPC),
+		dahboardsFolders:  NewDashboardsFoldersClient(apikeyCPC),
+		apiKeys:           NewApiKeysClient(apikeyCPC),
+		users:             NewUsersClient(apikeyCPC),
 	}
 }
--- go/go.mod
+++ go/go.mod
@@ -3,7 +3,6 @@ module coralogix-management-sdk/go
 go 1.22.4
 
 require (
-	github.com/grafana/grafana-api-golang-client v0.27.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20231002182017-d307bd883b97
 	google.golang.org/grpc v1.60.0
@@ -12,7 +11,7 @@ require (
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/net v0.16.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
