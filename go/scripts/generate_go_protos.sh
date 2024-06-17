e2c9acc7b463fe9c140b0ba6d8d1646a18204e62
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-06-17 13:33:27 +0200

    finally made it compile

--- go/scripts/generate_go_protos.sh
+++ go/scripts/generate_go_protos.sh
@@ -2,18 +2,29 @@
 
 # Directory containing the protobuf files
 proto_dir="../proto"
+go_out_dir="internal"
+mod_name="coralogix-management-sdk/go"
 proto_files=($(find "$proto_dir" -name "*.proto" -print))
 args=""
 # Build arguments for import paths of all modules
 for proto_file in "${proto_files[@]}" 
 do
     out_module=$(dirname $proto_file)
-    # out_module=${out_module/coralogixapis/coralogix}
+  
     if [[ $out_module == *"coralogix"* ]]; then
         mod_path="${out_module##*/com/}"
-        args+="--go_opt=M${proto_file##*$proto_dir/}=coralogix-management-sdk/go/internal/${mod_path} "
-        args+="--go-grpc_opt=M${proto_file##*$proto_dir/}=coralogix-management-sdk/go/internal/${mod_path} "
-
+        case "$mod_path" in
+            *dashboards/v1/ast/widgets/common/queries.proto ) 
+            ;; 
+            *dashboards* )
+                args+="--go_opt=M${proto_file##*$proto_dir/}=${mod_name}/${go_out_dir}/coralogixapis/dashboards/v1 "
+                args+="--go-grpc_opt=M${proto_file##*$proto_dir/}=${mod_name}/${go_out_dir}/coralogixapis/dashboards/v1 "
+                ;;
+            *)    
+                args+="--go_opt=M${proto_file##*$proto_dir/}=${mod_name}/${go_out_dir}/${mod_path} "
+                args+="--go-grpc_opt=M${proto_file##*$proto_dir/}=${mod_name}/${go_out_dir}/${mod_path} "
+                ;;
+        esac
     fi
 done
 
@@ -21,6 +32,16 @@ done
 for proto_file in "${proto_files[@]}" 
 do
     if [[ $proto_file == *"coralogix"* ]]; then
-        protoc --proto_path=$proto_dir --go_out=../.. --go-grpc_out=../.. $args $proto_file
+        case "$proto_file" in
+            *dashboards/v1/ast/widgets/common/queries.proto ) 
+            ;; 
+        *) 
+            protoc --proto_path=$proto_dir --go_out=../.. --go-grpc_out=../.. $args $proto_file
+            ;;
+        esac
     fi
 done
+
+sed -i'.bak' -e 's/file_com_coralogixapis_dashboards_v1_ast_widgets_common_queries_proto_init()/\/\/file_com_coralogixapis_dashboards_v1_ast_widgets_common_queries_proto_init()/g' ${go_out_dir}/coralogixapis/dashboards/v1/*
+
+rm -rf ${go_out_dir}/coralogixapis/dashboards/v1/*.bak
