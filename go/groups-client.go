b07392963ab30c861de2ed002b8f6308e1f7d3b1
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-08-27 11:41:46 +0200

    fix: re-exported groups types

--- go/groups-client.go
+++ go/groups-client.go
@@ -19,72 +19,84 @@ import (
 	groups "github.com/coralogix/coralogix-management-sdk/go/internal/coralogix/permissions/v1"
 )
 
-// TeamGroupID is the ID of a team group.
-type TeamGroupID = groups.TeamGroupId
-
-// GroupScope is a type for a group scope.
-type GroupScope = groups.Scope
-
-// TeamGroup is a type for a group.
-type TeamGroup = groups.TeamGroup
-
-// GroupsUser is a type for a user in a Groups context.
-type GroupsUser = groups.User
-
-// UpdateTeamGroupRequestRoleUpdates is a type for a request.
-type UpdateTeamGroupRequestRoleUpdates = groups.UpdateTeamGroupRequest_RoleUpdates
-
-// UpdateTeamGroupRequestUserUpdates is a type for a request.
-type UpdateTeamGroupRequestUserUpdates = groups.UpdateTeamGroupRequest_UserUpdates
-
-// GetTeamGroupRequest is a type for a request.
-type GetTeamGroupRequest = groups.GetTeamGroupRequest
-
-// GetTeamGroupByNameRequest is a type for a request.
-type GetTeamGroupByNameRequest = groups.GetTeamGroupByNameRequest
-
-// GetTeamGroupsRequest is a type for a request.
-type GetTeamGroupsRequest = groups.GetTeamGroupsRequest
-
-// CreateTeamGroupRequest is a type for a request.
+// CreateTeamGroupRequest is a request to create a new group
 type CreateTeamGroupRequest = groups.CreateTeamGroupRequest
 
-// UpdateTeamGroupRequest is a type for a request.
-type UpdateTeamGroupRequest = groups.UpdateTeamGroupRequest
+// CreateTeamGroupResponse is a response to creating a new group
+type CreateTeamGroupResponse = groups.CreateTeamGroupResponse
 
-// DeleteTeamGroupRequest is a type for a request.
+// DeleteTeamGroupRequest is a request to delete a group by its ID
 type DeleteTeamGroupRequest = groups.DeleteTeamGroupRequest
 
-// GetGroupUsersRequest is a type for a request.
-type GetGroupUsersRequest = groups.GetGroupUsersRequest
+// UpdateTeamGroupRequest is a request to update a group by its ID
+type UpdateTeamGroupRequest = groups.UpdateTeamGroupRequest
 
-// AddUsersToTeamGroupRequest is a type for a request.
-type AddUsersToTeamGroupRequest = groups.AddUsersToTeamGroupRequest
+// GetTeamGroupRequest is a request to get a group by its ID
+type GetTeamGroupRequest = groups.GetTeamGroupRequest
 
-// AddUsersToTeamGroupsRequest is a type for a request.
-type AddUsersToTeamGroupsRequest = groups.AddUsersToTeamGroupsRequest
+// GetTeamGroupsRequest is a request to list all groups in the team
+type GetTeamGroupsRequest = groups.GetTeamGroupsRequest
 
-// RemoveUsersFromTeamGroupRequest is a type for a request.
-type RemoveUsersFromTeamGroupRequest = groups.RemoveUsersFromTeamGroupRequest
+// GetTeamGroupResponse is a response to getting a group by its ID
+type GetTeamGroupResponse = groups.GetTeamGroupResponse
 
-// RemoveUsersFromTeamGroupsRequest is a type for a request.
-type RemoveUsersFromTeamGroupsRequest = groups.RemoveUsersFromTeamGroupsRequest
+// GroupsTeamID is an ID for a group in a Team
+type GroupsTeamID = groups.TeamId
 
-// SetTeamGroupScopeRequest is a type for a request.
-type SetTeamGroupScopeRequest = groups.SetTeamGroupScopeRequest
+// GroupsTeamGroupID is an ID for a group in a Team
+type GroupsTeamGroupID = groups.TeamGroupId
 
-// GetTeamGroupScopeRequest is a type for a request.
-type GetTeamGroupScopeRequest = groups.GetTeamGroupScopeRequest
+// GroupsRoleID is an ID for a group in a Team
+type GroupsRoleID = groups.RoleId
 
-// RPC Values
+// GroupsUserID is an ID for a user
+type GroupsUserID = groups.UserId
+
+// AddUsersToGroup for bulk adding users to groups
+type AddUsersToGroup = groups.AddUsersToTeamGroupsRequest_AddUsersToTeamGroup
+
+// RemoveUsersFromGroup for bulk removing users from groups
+type RemoveUsersFromGroup = groups.RemoveUsersFromTeamGroupsRequest_RemoveUsersFromTeamGroup
+
 const (
-	CreateTeamGroupRPC          = groups.TeamPermissionsMgmtService_CreateTeamGroup_FullMethodName
-	GetTeamGroupRPC             = groups.TeamPermissionsMgmtService_GetTeamGroup_FullMethodName
-	GetTeamGroupsRPC            = groups.TeamPermissionsMgmtService_GetTeamGroups_FullMethodName
-	UpdateTeamGroupRPC          = groups.TeamPermissionsMgmtService_UpdateTeamGroup_FullMethodName
-	DeleteTeamGroupRPC          = groups.TeamPermissionsMgmtService_DeleteTeamGroup_FullMethodName
-	AddUsersToTeamGroupRPC      = groups.TeamPermissionsMgmtService_AddUsersToTeamGroup_FullMethodName
-	RemoveUsersFromTeamGroupRPC = groups.TeamPermissionsMgmtService_RemoveUsersFromTeamGroup_FullMethodName
+	// GetTeamGroupRpc is the name of the RPC to get a group by its ID
+	GetTeamGroupRpc = groups.TeamPermissionsMgmtService_GetTeamGroup_FullMethodName
+
+	// GetTeamGroupsRpc is the name of the RPC to list all groups in the team
+	GetTeamGroupByNameRpc = groups.TeamPermissionsMgmtService_GetTeamGroupByName_FullMethodName
+
+	// GetTeamGroupsRpc is the name of the RPC to list all groups in the team
+	GetTeamGroupsRpc = groups.TeamPermissionsMgmtService_GetTeamGroups_FullMethodName
+
+	// CreateTeamGroupRpc is the name of the RPC to create a new group
+	CreateTeamGroupRpc = groups.TeamPermissionsMgmtService_CreateTeamGroup_FullMethodName
+
+	// UpdateTeamGroupRpc is the name of the RPC to update a group
+	UpdateTeamGroupRpc = groups.TeamPermissionsMgmtService_UpdateTeamGroup_FullMethodName
+
+	// DeleteTeamGroupRpc is the name of the RPC to delete a group by its ID
+	DeleteTeamGroupRpc = groups.TeamPermissionsMgmtService_DeleteTeamGroup_FullMethodName
+
+	// GetGroupUsersRpc is the name of the RPC to get all users in a group
+	GetGroupUsersRpc = groups.TeamPermissionsMgmtService_GetGroupUsers_FullMethodName
+
+	// AddUsersToTeamGroupRpc is the name of the RPC to add users to a group
+	AddUsersToTeamGroupRpc = groups.TeamPermissionsMgmtService_AddUsersToTeamGroup_FullMethodName
+
+	// AddUsersToTeamGroupsRpc is the name of the RPC to remove users from a group
+	AddUsersToTeamGroupsRpc = groups.TeamPermissionsMgmtService_AddUsersToTeamGroups_FullMethodName
+
+	// RemoveUsersFromTeamGroupRpc is the name of the RPC to remove users from a group
+	RemoveUsersFromTeamGroupRpc = groups.TeamPermissionsMgmtService_RemoveUsersFromTeamGroup_FullMethodName
+
+	// RemoveUsersFromTeamGroupsRpc is the name of the RPC to remove users from multiple groups
+	RemoveUsersFromTeamGroupsRpc = groups.TeamPermissionsMgmtService_RemoveUsersFromTeamGroups_FullMethodName
+
+	// SetTeamGroupScopeRpc is the name of the RPC to set the scope of a group
+	SetTeamGroupScopeRpc = groups.TeamPermissionsMgmtService_SetTeamGroupScope_FullMethodName
+
+	// GetTeamGroupScopeRpc is the name of the RPC to get the scope of a group
+	GetTeamGroupScopeRpc = groups.TeamPermissionsMgmtService_GetTeamGroupScope_FullMethodName
 )
 
 // GroupsClient is a client for the Groups API
@@ -188,6 +200,34 @@ func (c GroupsClient) RemoveUsers(ctx context.Context, req *groups.RemoveUsersFr
 	client := groups.NewTeamPermissionsMgmtServiceClient(conn)
 
 	return client.RemoveUsersFromTeamGroup(callProperties.Ctx, req, callProperties.CallOptions...)
+}
+
+// AddUsersBulk adds users to a group
+func (c GroupsClient) AddUsersBulk(ctx context.Context, req *groups.AddUsersToTeamGroupsRequest) (*groups.AddUsersToTeamGroupsResponse, error) {
+	callProperties, err := c.callPropertiesCreator.GetTeamsLevelCallProperties(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	conn := callProperties.Connection
+	defer conn.Close()
+	client := groups.NewTeamPermissionsMgmtServiceClient(conn)
+
+	return client.AddUsersToTeamGroups(callProperties.Ctx, req, callProperties.CallOptions...)
+}
+
+// RemoveUsers removes users from a group
+func (c GroupsClient) RemoveUsersBulk(ctx context.Context, req *groups.RemoveUsersFromTeamGroupsRequest) (*groups.RemoveUsersFromTeamGroupsResponse, error) {
+	callProperties, err := c.callPropertiesCreator.GetTeamsLevelCallProperties(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	conn := callProperties.Connection
+	defer conn.Close()
+	client := groups.NewTeamPermissionsMgmtServiceClient(conn)
+
+	return client.RemoveUsersFromTeamGroups(callProperties.Ctx, req, callProperties.CallOptions...)
 }
 
 // NewGroupsClient creates a new GroupsClient
