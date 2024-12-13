d02a75b7bf5585d46d1edc0dd4bf627291de37c6
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-12-13 13:43:26 +0100

    fix: auto copy in build.rs

--- rust/cx-api/build.rs
+++ rust/cx-api/build.rs
@@ -12,7 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::path::PathBuf;
+use std::{
+    fs,
+    io,
+    path::{
+        Path,
+        PathBuf,
+    },
+};
+const PROTOS_DIR: &str = "proto";
+
+/// Copy files from source to destination recursively.
+pub fn copy_recursively(source: impl AsRef<Path>, destination: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&destination)?;
+    for entry in fs::read_dir(source)? {
+        let entry = entry?;
+        let filetype = entry.file_type()?;
+        if filetype.is_dir() {
+            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), destination.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=build.rs");
@@ -20,19 +43,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=protofetch.toml");
     println!("cargo:rerun-if-changed=protofetch.lock");
 
-    let is_ci: bool = std::env::var("CI")
-        .unwrap_or("false".into())
-        .parse()
-        .unwrap();
-
-    let root = if is_ci {
-        format!("{}/rust/cx-api", std::env::var("GITHUB_WORKSPACE").unwrap())
-    } else {
-        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
-            .into_os_string()
-            .into_string()
-            .unwrap()
-    };
+    let mut project_root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    project_root.pop();
+    project_root.pop();
+    project_root.push(PROTOS_DIR);
+    let mut rust_root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    rust_root.push(PROTOS_DIR);
+    println!(
+        "project_root: {:?}, rust_root: {:?}",
+        project_root, rust_root
+    );
+    if !rust_root.exists() {
+        println!("copying proto files from the project root to the rust root");
+        copy_recursively(project_root, &rust_root).unwrap()
+    }
+    let root = rust_root.into_os_string().into_string().unwrap();
 
     println!("project_root: {:?}", root);
     let building = &[
@@ -102,41 +127,38 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .include_file("coralogix_management_api_grpc.rs")
         .build_server(false)
         .build_client(true)
-        .compile_protos(building, &[format!("{}/proto/", root)])?;
+        .compile_protos(building, &[format!("{}/", root)])?;
     Ok(())
 }
 
 fn tags_service(root: &str) -> Vec<String> {
-    vec![format!(
-        "{}/proto/com/coralogix/tags/v1/tag_service.proto",
-        root
-    )]
+    vec![format!("{}/com/coralogix/tags/v1/tag_service.proto", root)]
 }
 
 fn alerts_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/alerts/v3/alert_defs_service.proto",
+        "{}/com/coralogixapis/alerts/v3/alert_defs_service.proto",
         root
     )]
 }
 
 fn alerts_scheduler_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/alerting/alert_scheduler_rule_protobuf/v1/alert_scheduler_rule_service.proto",
+        "{}/com/coralogixapis/alerting/alert_scheduler_rule_protobuf/v1/alert_scheduler_rule_service.proto",
         root
     )]
 }
 
 fn rules_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/rules/v1/rule_groups_service.proto",
+        "{}/com/coralogix/rules/v1/rule_groups_service.proto",
         root
     )]
 }
 
 fn outgoing_webhooks_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/outgoing_webhooks/v1/outgoing_webhook_service.proto",
+        "{}/com/coralogix/outgoing_webhooks/v1/outgoing_webhook_service.proto",
         root
     )]
 }
@@ -144,11 +166,11 @@ fn outgoing_webhooks_service(root: &str) -> Vec<String> {
 fn enrichment_service(root: &str) -> Vec<String> {
     vec![
         format!(
-            "{}/proto/com/coralogix/enrichment/v1/enrichment_service.proto",
+            "{}/com/coralogix/enrichment/v1/enrichment_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogix/enrichment/v1/custom_enrichment_service.proto",
+            "{}/com/coralogix/enrichment/v1/custom_enrichment_service.proto",
             root
         ),
     ]
@@ -156,28 +178,28 @@ fn enrichment_service(root: &str) -> Vec<String> {
 
 fn data_usage_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/datausage/v2/data_usage_service.proto",
+        "{}/com/coralogix/datausage/v2/data_usage_service.proto",
         root
     )]
 }
 
 fn quota_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/quota/v1/policies_service.proto",
+        "{}/com/coralogix/quota/v1/policies_service.proto",
         root
     )]
 }
 
 fn e2m_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/events2metrics/v2/events2metrics_service.proto",
+        "{}/com/coralogixapis/events2metrics/v2/events2metrics_service.proto",
         root
     )]
 }
 
 fn recording_rule_group_sets_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/metrics-rule-manager/v1/groups.proto",
+        "{}/com/coralogixapis/metrics-rule-manager/v1/groups.proto",
         root
     )]
 }
@@ -185,43 +207,43 @@ fn recording_rule_group_sets_service(root: &str) -> Vec<String> {
 fn dashboards_service(root: &str) -> Vec<String> {
     vec![
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/archive_logs_data_source_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/archive_logs_data_source_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/archive_spans_data_source_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/archive_spans_data_source_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/dashboard_catalog_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/dashboard_catalog_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/dashboard_folders_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/dashboard_folders_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/dashboards_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/dashboards_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/dataprime_data_source_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/dataprime_data_source_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/logs_data_source_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/logs_data_source_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/metrics_data_source_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/metrics_data_source_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/spans_data_source_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/spans_data_source_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/dashboards/v1/services/team_settings_service.proto",
+            "{}/com/coralogixapis/dashboards/v1/services/team_settings_service.proto",
             root
         ),
     ]
@@ -229,7 +251,7 @@ fn dashboards_service(root: &str) -> Vec<String> {
 
 fn dataprime_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/dataprime/v1/query_service.proto",
+        "{}/com/coralogixapis/dataprime/v1/query_service.proto",
         root
     )]
 }
@@ -237,11 +259,11 @@ fn dataprime_service(root: &str) -> Vec<String> {
 fn views_service(root: &str) -> Vec<String> {
     vec![
         format!(
-            "{}/proto/com/coralogixapis/views/v1/services/views_service.proto",
+            "{}/com/coralogixapis/views/v1/services/views_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/views/v1/services/views_folders_service.proto",
+            "{}/com/coralogixapis/views/v1/services/views_folders_service.proto",
             root
         ),
     ]
@@ -249,97 +271,91 @@ fn views_service(root: &str) -> Vec<String> {
 
 fn actions_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/actions/v2/actions_service.proto",
+        "{}/com/coralogixapis/actions/v2/actions_service.proto",
         root
     )]
 }
 
 fn api_keys_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/aaa/apikeys/v3/api_keys.proto",
+        "{}/com/coralogixapis/aaa/apikeys/v3/api_keys.proto",
         root
     )]
 }
 
 fn target_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/archive/v2/target_service.proto",
+        "{}/com/coralogix/archive/v2/target_service.proto",
         root
     )]
 }
 
 fn metrics_configurator_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/metrics/metrics-configurator.proto",
+        "{}/com/coralogix/metrics/metrics-configurator.proto",
         root
     )]
 }
 
 fn retention_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/archive/v1/retentions_service.proto",
+        "{}/com/coralogix/archive/v1/retentions_service.proto",
         root
     )]
 }
 
 fn team_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/aaa/organisations/v2/team_service.proto",
+        "{}/com/coralogixapis/aaa/organisations/v2/team_service.proto",
         root
     )]
 }
 
 fn scopes_service(root: &str) -> Vec<String> {
-    vec![format!(
-        "{}/proto/com/coralogixapis/scopes/v1/scopes.proto",
-        root
-    )]
+    vec![format!("{}/com/coralogixapis/scopes/v1/scopes.proto", root)]
 }
 
 fn slos_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogixapis/apm/services/v1/service_slo_service.proto",
+        "{}/com/coralogixapis/apm/services/v1/service_slo_service.proto",
         root
     )]
 }
 
 fn groups_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/permissions/v1/team_permissions_mgmt_service.proto",
+        "{}/com/coralogix/permissions/v1/team_permissions_mgmt_service.proto",
         root
     )]
 }
 
 fn integrations_service(root: &str) -> Vec<String> {
     vec![format!(
-        "{}/proto/com/coralogix/integrations/v1/integration_service.proto",
+        "{}/com/coralogix/integrations/v1/integration_service.proto",
         root
     )]
 }
 
 fn saml_configuration_service(root: &str) -> Vec<String> {
-    vec![format!(
-        "{}/proto/com/coralogixapis/aaa/sso/v2/saml.proto",
-        root
-    )]
+    vec![format!("{}/com/coralogixapis/aaa/sso/v2/saml.proto", root)]
 }
 
 fn notifications_service(root: &str) -> Vec<String> {
     vec![
         format!(
-            "{}/proto/com/coralogixapis/notification_center/connectors/v1/connectors_service.proto",
+            "{}/com/coralogixapis/notification_center/connectors/v1/connectors_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/notification_center/entities/v1/entities_service.proto",
+            "{}/com/coralogixapis/notification_center/entities/v1/entities_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/notification_center/notifications/v1/testing_service.proto",
+            "{}/com/coralogixapis/notification_center/notifications/v1/testing_service.proto",
             root
         ),
         format!(
-            "{}/proto/com/coralogixapis/notification_center/presets/v1/presets_service.proto",
+            "{}/com/coralogixapis/notification_center/presets/v1/presets_service.proto",
             root
         ),
     ]
