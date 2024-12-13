7696a51071d92e70dc48f846adc26cb7395ca6c2
Claus Matzinger <claus.matzinger+kb@gmail.com>
2024-12-13 14:19:13 +0100

    fix: auto copy in build.rs

--- rust/cx-api/build.rs
+++ rust/cx-api/build.rs
@@ -56,9 +56,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let project_root_created = project_root.metadata().unwrap().created().ok();
     let source_is_newer = !rust_root.exists()
-        || project_root_created.map_or(false, |c| {
-            rust_root.metadata().unwrap().created().unwrap() < c
-        });
+        || project_root_created
+            .is_some_and(|c| rust_root.metadata().unwrap().created().unwrap() < c);
     if source_is_newer {
         println!("copying proto files from the project root to the rust root");
         copy_recursively(project_root, &rust_root).unwrap()
