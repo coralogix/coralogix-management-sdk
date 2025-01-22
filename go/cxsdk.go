102b0342c9f316206861981e3606a44d007caacc
Assaf Admi <assaf.admi@coralogix.com>
2025-01-22 17:26:22 +0200

    Fix domain issue (#154)

--- go/cxsdk.go
+++ go/cxsdk.go
@@ -287,13 +287,12 @@ func CoralogixGrpcEndpointFromRegion(regionIdentifier string) string {
 	case "ap3":
 		return GrpcAP3
 	default:
-		return regionIdentifier
+		return fmt.Sprintf("ng-api-grpc.%s:443", regionIdentifier)
 	}
 }
 
 // CoralogixRestEndpointFromRegion reads the Coralogix REST endpoint from environment variables.
 func CoralogixRestEndpointFromRegion(regionIdentifier string) string {
-
 	switch regionIdentifier {
 	case "us1":
 		return RestUS1
@@ -310,7 +309,7 @@ func CoralogixRestEndpointFromRegion(regionIdentifier string) string {
 	case "ap3":
 		return RestAP3
 	default:
-		return regionIdentifier
+		return fmt.Sprintf("https://ng-api-http.%s", regionIdentifier)
 	}
 }
 
