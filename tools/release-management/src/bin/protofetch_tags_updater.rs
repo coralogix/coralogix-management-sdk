1d601b8500142316019151a6d54d1d439539be9b
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-02-27 08:53:03 +0100

    fix: workflow and tags updater (#193)
    * fix: workflow and tags updater

--- tools/release-management/src/bin/protofetch_tags_updater.rs
+++ tools/release-management/src/bin/protofetch_tags_updater.rs
@@ -12,13 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::path::PathBuf;
+use std::{path::PathBuf, process::Output};
 
 use clap::Parser;
 use git_cmd::Repo;
-use protofetch::Protofetch;
 use time::format_description;
 use toml_edit::{value, DocumentMut};
+
+
+const GO_DIR: &str = "../../go";
 
 const PROTOFETCH_META_KEYS: [&str; 3] = ["name", "description", "proto_out_dir"];
 
@@ -139,20 +141,16 @@ async fn main() -> eyre::Result<()> {
         return Ok(());
     }
 
-    // Protofetch updating
-    tokio::fs::write(&protofetch_path, protofetch_descriptor.to_string()).await?;
-    tokio::fs::remove_dir_all(&proto_dir).await?;
 
-    let protos = Protofetch::builder()
-        .module_file_name(&protofetch_path)
-        .output_directory_name(&proto_dir)
-        .lock_file_name(&protolock_path)
-        .try_build()
-        .expect("Protofetch failed");
+    // Make: protofetch & build Go proxies
+    let output = tokio::process::Command::new("make")
+        .arg("proto-go-generate")
+        .current_dir(&args.root)
+        .output().await.unwrap();
 
-    protos
-        .fetch(protofetch::LockMode::Recreate)
-        .expect("Protofetch failed");
+    if !output.status.success() {
+        println!("Error calling make: {}", String::from_utf8_lossy(&output.stderr))
+    }
 
     // Git operations
     let repo = Repo::new(&args.git_dir)
@@ -171,6 +169,7 @@ async fn main() -> eyre::Result<()> {
         protofetch_path.into_os_string().to_str().unwrap(),
         protolock_path.into_os_string().to_str().unwrap(),
         proto_dir.into_os_string().to_str().unwrap(),
+        GO_DIR,
     ])
     .expect("Failed to add files");
     repo.commit("chore: Update protofetch.toml")
