b4c8ba65de5d9cc85d5f2b60daf0931bff1ee267
Claus Matzinger <claus.matzinger+kb@gmail.com>
2025-03-21 16:40:04 +0100

    docs: added doc links

--- rust/cx-sdk/src/client/teams.rs
+++ rust/cx-sdk/src/client/teams.rs
@@ -61,7 +61,6 @@ const TEAMS_FEATURE_GROUP_ID: &str = "aaa";
 
 /// The Team API client.
 /// Read more at https://coralogix.com/docs/user-team-management/ and https://coralogix.com/docs/user-guides/account-management/payment-and-billing/quota-management/
-/// 
 pub struct TeamsClient {
     metadata_map: MetadataMap,
     service_client: Mutex<TeamServiceClient<Channel>>,
