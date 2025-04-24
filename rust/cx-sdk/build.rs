9bffb2c4aaddd874ce3442cab8dbe78640f01375
Roman Timushev <rtimush@gmail.com>
2025-04-24 10:31:07 +0300

    Fix incremental rust build (#267)
    * Fix incremental rust build
    
    * Fix clippy warning
    
    ---------
    
    Co-authored-by: Luigi Taglialatela <luigi.taglialatela1996@gmail.com>

--- rust/cx-sdk/build.rs
+++ rust/cx-sdk/build.rs
@@ -28,21 +28,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .spawn();
 
     if rustc_version_command.is_err() {
-        println!("cargo:rustc-env=RUSTC_VERSION={}", DEFAULT_VERSION_STRING);
+        println!("cargo:rustc-env=RUSTC_VERSION={DEFAULT_VERSION_STRING}");
         return Ok(());
     }
 
     let rustc_version_output = rustc_version_command.unwrap().wait_with_output();
 
     if rustc_version_output.is_err() {
-        println!("cargo:rustc-env=RUSTC_VERSION={}", DEFAULT_VERSION_STRING);
+        println!("cargo:rustc-env=RUSTC_VERSION={DEFAULT_VERSION_STRING}");
         return Ok(());
     }
 
     let rust_version_string = String::from_utf8(rustc_version_output.unwrap().stdout);
 
     if rust_version_string.is_err() {
-        println!("cargo:rustc-env=RUSTC_VERSION={}", DEFAULT_VERSION_STRING);
+        println!("cargo:rustc-env=RUSTC_VERSION={DEFAULT_VERSION_STRING}");
         return Ok(());
     }
 
