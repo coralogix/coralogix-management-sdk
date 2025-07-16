51c0a5dc12172693f5e514ca8caeefbd6f60c107
Or Novogroder <or.novogroder@coralogix.com>
2025-07-16 17:14:27 +0300

    expose ScopeFilters

--- go/groups-client.go
+++ go/groups-client.go
@@ -34,6 +34,9 @@ type GroupsUser = groups.User
 // RoleID is a type for a role ID to use in a group.
 type RoleID = groups.RoleId
 
+// ScopeFilters is a type for scope filters used in group operations.
+type ScopeFilters = groups.ScopeFilters
+
 // Role is a type for a role in a group.
 type Role = groups.Role
 
