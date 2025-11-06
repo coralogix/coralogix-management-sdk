ad47a32c3cece3e41ca51fa76e57e32051530a43
assaf.admi <assaf.admi@coralogix.com>
2025-11-06 10:28:02 +0200

    slo

--- go/openapi/examples/slos_test.go
+++ go/openapi/examples/slos_test.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Coralogix Ltd.
+// Copyright 2025 Coralogix Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,7 +6,7 @@
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing,
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,89 +14,99 @@
 
 package examples
 
-//
-//import (
-//	"context"
-//	"fmt"
-//	"testing"
-//
-//	"github.com/google/uuid"
-//	"github.com/stretchr/testify/assert"
-//
-//	"github.com/coralogix/coralogix-management-sdk/go/openapi/cxsdk"
-//	slos "github.com/coralogix/coralogix-management-sdk/go/openapi/gen/slo_service"
-//)
-//
-//func TestSlos(t *testing.T) {
-//	t.Skip("Unstable Test")
-//	cpc := cxsdk.NewSDKCallPropertiesCreator(
-//		cxsdk.URLFromRegion(cxsdk.RegionFromEnv()),
-//		cxsdk.APIKeyFromEnv(),
-//	)
-//
-//	client := cxsdk.NewSLOsClient(cpc)
-//	name := fmt.Sprintf("coralogix_slo_go_example_%v", uuid.NewString())
-//
-//	slo := slos.ServiceSlo{
-//		Name:                      name,
-//		Description:               slos.PtrString("Example SLO created via Go SDK"),
-//		TargetThresholdPercentage: 30,
-//		RequestBasedMetricSli: &slos.RequestBasedMetricSli{
-//			GoodEvents: slos.Metric{
-//				Query: "avg(rate(cpu_usage_seconds_total[5m])) by (instance)",
-//			},
-//			TotalEvents: slos.Metric{
-//				Query: "avg(rate(cpu_usage_seconds_total[5m])) by (instance)",
-//			},
-//		},
-//		SloTimeFrame: slos.PtrString("SLO_TIME_FRAME_7_DAYS"),
-//		Labels: []slos.SloLabelsInner{
-//			{Key: slos.PtrString("label1"), Value: slos.PtrString("value1")},
-//		},
-//	}
-//
-//	req := slos.CreateServiceSLORequest{
-//		Slo: &slo,
-//	}
-//
-//	createResp, httpResp, err := client.
-//		ServiceSloServiceCreateServiceSlo(context.Background()).
-//		CreateServiceSLORequest(req).
-//		Execute()
-//	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
-//
-//	sloID := createResp.Slo.Slo.Id
-//	assert.NotNil(t, sloID)
-//
-//	got, httpResp, err := client.
-//		SlosServiceGetSlo(context.Background(), *sloID).
-//		Execute()
-//	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
-//	assert.Equal(t, name, got.Slo.Slo.Name)
-//
-//	updatedName := name + "_updated"
-//	slo.Id = sloID
-//	slo.Revision = createResp.Slo.Slo.Revision
-//	slo.Name = updatedName
-//
-//	req = slos.SlosServiceReplaceSloRequest{
-//		Slo2: &slo,
-//	}
-//	updateResp, httpResp, err := client.
-//		SlosServiceReplaceSlo(context.Background()).
-//		SlosServiceReplaceSloRequest(req).
-//		Execute()
-//	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
-//	assert.Equal(t, updatedName, updateResp.Slo.Slo.Name)
-//
-//	listResp, httpResp, err := client.
-//		SlosServiceListSlos(context.Background()).
-//		Execute()
-//	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
-//	assert.Greater(t, len(listResp.Slos), 0)
-//
-//	_, httpResp, err = client.
-//		SlosServiceDeleteSlo(context.Background(), *sloID).
-//		Execute()
-//	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
-//}
+import (
+	"context"
+	"testing"
+
+	"github.com/coralogix/coralogix-management-sdk/go/openapi/cxsdk"
+	slos "github.com/coralogix/coralogix-management-sdk/go/openapi/gen/slos_service"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSLOs(t *testing.T) {
+	ctx := context.Background()
+	cpc := cxsdk.NewSDKCallPropertiesCreator(
+		cxsdk.URLFromRegion(cxsdk.RegionFromEnv()),
+		cxsdk.APIKeyFromEnv(),
+	)
+	client := cxsdk.NewClientSet(cpc).SLOs()
+
+	sloName := "example_slo_" + uuid.NewString()
+	createReq := slos.SlosServiceCreateSloRequest{
+		SloRequestBasedMetricSli: &slos.SloRequestBasedMetricSli{
+			Name:                      sloName,
+			Description:               slos.PtrString("example slo created via openapi sdk"),
+			TargetThresholdPercentage: 30,
+			RequestBasedMetricSli: &slos.RequestBasedMetricSli{
+				GoodEvents: slos.Metric{
+					Query: "avg(rate(cpu_usage_seconds_total[5m])) by (instance)",
+				},
+				TotalEvents: slos.Metric{
+					Query: "avg(rate(cpu_usage_seconds_total[5m])) by (instance)",
+				},
+			},
+			SloTimeFrame: slos.SLOTIMEFRAME_SLO_TIME_FRAME_7_DAYS.Ptr(),
+			Labels:       &map[string]string{"label1": "value1"},
+		},
+	}
+
+	createResp, httpResp, err := client.
+		SlosServiceCreateSlo(ctx).
+		SlosServiceCreateSloRequest(createReq).
+		Execute()
+	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
+
+	sloID := createResp.GetSlo().SloRequestBasedMetricSli.GetId()
+	getResp, httpResp, err := client.
+		SlosServiceGetSlo(ctx, sloID).
+		Execute()
+	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
+	require.Equal(t, sloName, getResp.GetSlo().SloRequestBasedMetricSli.GetName())
+
+	updatedName := sloName + "_updated"
+	updateReq := slos.SlosServiceReplaceSloRequest{
+		SloRequestBasedMetricSli: &slos.SloRequestBasedMetricSli{
+			Id:                        &sloID,
+			Name:                      updatedName,
+			Description:               slos.PtrString("example slo created via openapi sdk"),
+			TargetThresholdPercentage: 30,
+			RequestBasedMetricSli: &slos.RequestBasedMetricSli{
+				GoodEvents: slos.Metric{
+					Query: "avg(rate(cpu_usage_seconds_total[5m])) by (instance)",
+				},
+				TotalEvents: slos.Metric{
+					Query: "avg(rate(cpu_usage_seconds_total[5m])) by (instance)",
+				},
+			},
+			SloTimeFrame: slos.SLOTIMEFRAME_SLO_TIME_FRAME_7_DAYS.Ptr(),
+			Labels:       &map[string]string{"label1": "value1"},
+		},
+	}
+
+	updateResp, httpResp, err := client.
+		SlosServiceReplaceSlo(ctx).
+		SlosServiceReplaceSloRequest(updateReq).
+		Execute()
+	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
+	require.Equal(t, updatedName, updateResp.GetSlo().SloRequestBasedMetricSli.GetName())
+
+	listResp, httpResp, err := client.
+		SlosServiceListSlos(ctx).
+		Execute()
+	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
+
+	found := false
+	for _, s := range listResp.GetSlos() {
+		if s.SloRequestBasedMetricSli.GetId() == sloID {
+			found = true
+			break
+		}
+	}
+	require.True(t, found, "created SLO not found in list response")
+
+	_, httpResp, err = client.
+		SlosServiceDeleteSlo(ctx, sloID).
+		Execute()
+	assertNilAndPrintError(t, cxsdk.NewAPIError(httpResp, err))
+}
